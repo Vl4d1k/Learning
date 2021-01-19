0df90ac807f035cfdcc9fb0743ee6448e5ef30da
Vlad Stelmakh <vlad1k9369@gmail.com>
2021-01-20 00:23:47 +0300

    map dialogs items

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,4 +1,13 @@
-const MessageItem = (props) => {
+const DialogsData = [
+    { message: "Hey, Joel, I here to help you out please tell me", time: "11:26", name: "Jessica Koel", photo: "https://i.imgur.com/aq39RMA.jpg"},
+    { message: "I will send you all documents as soon as possible", time: "12:26", name: "Komeial Bolger", photo: "https://i.imgur.com/eMaYwXn.jpg" },
+    { message: "Are you going to business trip next week", time: "8:26", name: "Tamaara Suiale", photo: "https://i.imgur.com/zQZSWrt.jpg" },
+    { message: "I suggest to start new business soon", time: "17:19", name: "Sam Nielson", photo: "https://i.imgur.com/agRGhBc.jpg" },
+    { message: "We need to start new reseatch center.", time: "16:56", name: "Caroline Nexon", photo: "https://i.imgur.com/uIgDDDd.jpg" },
+    { message: "May be yes", time: "11:25", name: "Patrick Koeler", photo: "https://i.imgur.com/tT8rjKC.jpg" }
+]; 
+
+const DialogItem = (props) => {
     return (
         <li className="flex justify-between items-center bg-white mt-2 p-2 hover:shadow-lg rounded cursor-pointer transition">
             <div className="flex ml-2"> <img src={props.photo} width={40} height={40} className="rounded-full" alt="avatar"/>
@@ -10,6 +19,7 @@ const MessageItem = (props) => {
     )
     
 }
+
 const Messages = () => {
     return (
         <div className="py-10 h-screen w-full bg-gray-300 px-2">
@@ -20,17 +30,7 @@ const Messages = () => {
                         <div className="relative"> <input type="text" className="w-full h-12 rounded focus:outline-none px-3 focus:shadow-md" placeholder="Search..." /> <i className="fa fa-search absolute right-3 top-4 text-gray-300" /> </div>
                         <ul>
 
-                            <MessageItem  message="Hey, Joel, I here to help you out please tell me" time="11:26" name="Jessica Koel" photo="https://i.imgur.com/aq39RMA.jpg" />
-
-                            <MessageItem  message="I will send you all documents as soon as possible" time="12:26" name="Komeial Bolger" photo="https://i.imgur.com/eMaYwXn.jpg" />
-
-                            <MessageItem  message="Are you going to business trip next week" time="8:26" name="Tamaara Suiale" photo="https://i.imgur.com/zQZSWrt.jpg" />
-
-                            <MessageItem  message="I suggest to start new business soon" time="7:19" name="Sam Nielson" photo="https://i.imgur.com/agRGhBc.jpg" />
-
-                            <MessageItem  message="We need to start new reseatch center." time="16:56" name="Caroline Nexon" photo="https://i.imgur.com/uIgDDDd.jpg" />
-
-                            <MessageItem  message="May be yes" time="11:25" name="Patrick Koeler" photo="https://i.imgur.com/tT8rjKC.jpg" />
+                            {DialogsData.map( dialog => ( <DialogItem  message={dialog.mess} time={dialog.time} name={dialog.name} photo={dialog.photo} />) )}                        
 
                         </ul>
                     </div>
