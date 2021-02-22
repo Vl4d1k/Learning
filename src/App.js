7c27f8e7dd275a69c3e3de4d72112aa6f708167c
Vlad Stelmakh <vlad1k9369@gmail.com>
2021-02-22 14:54:29 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -6,32 +6,29 @@ import Users from './components/UsersPage/UsersContainer'
 import DialogContainer from './components/MessagesPage/Dialog/DialogContainer';
 
 import './App.css';
+
 import { BrowserRouter, Route } from 'react-router-dom';
 
 const App = (props) => {
-  
-  return (
-    <BrowserRouter>
-      <div className="bg-gray-800 font-sans leading-normal tracking-normal">
 
-        <div className="flex flex-col md:flex-row" >
-          
-          <Sidebar />
-          
-          <Route path='/profile' render={ () => <Profile /> } />
-          <Route path='/messages' exact render={ () => <Messages /> }  />
-          <Route path='/users' component={Users}  />
-          
-          {/* <Route path='/users' render={ () => <Users /> } /> */}
+    return (
+    <BrowserRouter >
+        <div className="bg-gray-800 font-sans leading-normal tracking-normal" >
+            <div className="flex flex-col md:flex-row" >
+                <Sidebar />
 
-          <Route path={'/messages/:id'} exact render={ () => <DialogContainer /> }/>
-          
-        </div>
+                <Route path='/profile'render={() => < Profile />}/> 
+                <Route path='/messages' exact render={() => < Messages />}/> 
+                <Route path='/users' component={Users} />
+                { /* <Route path='/users' render={ () => <Users /> } /> */}
+                <Route path={'/messages/:id'} exact render={() => < DialogContainer />}/>
+            
+            </div>
 
-      </div>
+        </div> 
     </BrowserRouter>
-    
-  );
+
+    );
 }
 
 export default App;
